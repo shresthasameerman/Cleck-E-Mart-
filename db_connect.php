f0131690f2fd6b447ccc27018cf83e22162bdf33
Nikhilesh Maharjan <nikhil.maharjan123@gmail.com>
2026-05-18 23:19:30 +0545

    Changes to the db connect

--- db_connect.php
+++ db_connect.php
@@ -2,12 +2,12 @@
 // db_connect.php
 
 // 1. Enter the exact same Username and Password you just used in the VSCode extension
-$db_user = "CLECK"; 
+$db_user = "ADMIN";
 $db_pass = "Oracle123#Apex";
 
 // 2. The host string (localhost, port 1521, and your service name)
 // Change 'XEPDB1' to 'FREEPDB1' or 'XE' if you used a different service name earlier.
-$db_host = "localhost:1521/XEPDB1"; 
+$db_host = "localhost:1521/XEPDB1";
 
 // 3. Establish the connection using the native Oracle OCI8 function
 $conn = oci_connect($db_user, $db_pass, $db_host);
